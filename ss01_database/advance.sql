0b158436c216016155531a34e24ba7812898550b
phamthanhcan <90948086+phamthanhcan@users.noreply.github.com>
2022-03-24 08:43:02 +0700

    Update advance.sql

--- ss01_database/advance.sql
+++ ss01_database/advance.sql
@@ -49,13 +49,29 @@ delimiter //
 	begin 
 		declare total int;
         set total = (select count(*) from hop_dong);
-		SIGNAL SQLSTATE '27000' SET MESSAGE_TEXT = total;
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = total;
 	end;
 // delimiter ;
 
 drop trigger tr_xoa_hop_dong;
 
 delete from hop_dong where hop_dong.ma_hop_dong = 2;
+
+-- cau 26
+delimiter //
+create trigger tr_cap_nhat_hop_dong 
+before update
+on hop_dong for each row
+begin
+	if datediff(ngay_ket_thuc, ngay_lam_hop_dong) < 2 then 
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "Ngày kết thúc hợp đồng phải lớn hơn ngày làm hợp đồng ít nhất là 2 ngày";
+    end if;
+end;
+// delimiter ;
+
+drop trigger tr_cap_nhat_hop_dong;
+
+update hop_dong set ngay_ket_thuc = '2020-07-15' where ma_hop_dong = 2;
 
 -- cau 27
 -- cau a
@@ -80,13 +96,30 @@ select func_dem_dich_vu () as tong_dich_vu_sd_tren_2tr;
 -- cau b
 delimiter //
 create function func_tinh_thoi_gian_hop_dong(ma_kh int)
-returns int
+returns varchar(50)
 deterministic
 	begin
-		declare result int;
-        set result = concat((select max(datediff(ngay_ket_thuc, ngay_lam_hop_dong)) from hop_dong where ma_khach_hang = ma_kh), ' ngày');
+		declare result varchar(50);
+        set result = concat((select max(datediff(ngay_ket_thuc, ngay_lam_hop_dong)) from hop_dong where ma_khach_hang = ma_kh), ' ngày') ;
         return result;
     end;
 // delimiter ;
 
-select func_tinh_thoi_gian_hop_dong(5) as thoi_gian_hop_dong_dai_nhat
+select func_tinh_thoi_gian_hop_dong(5) as thoi_gian_hop_dong_dai_nhat;
+
+-- cau 28
+delimiter //
+create procedure sp_xoa_dich_vu_va_hd_room()
+begin
+	drop table if exists temp_table;
+    create table temp_table(ma_hop_dong int, ma_dich_vu int);
+    
+    insert into temp_table 
+	select dich_vu.ma_dich_vu, hop_dong.ma_hop_dong from dich_vu inner join hop_dong on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu 
+    and dich_vu.ma_loai_dich_vu = 3 and ngay_lam_hop_dong between '2015-01-01' and '2019-12-31';
+    
+    delete from hop_dong_chi_tiet where ma_hop_dong in (select ma_hop_dong from temp_table);
+    delete from hop_dong where ma_hop_dong in (select ma_hop_dong from temp_table);
+    delete from dich_vu where ma_dich_vu in (select ma_dich_vu from temp_table);
+end; 
+// delimiter ;
